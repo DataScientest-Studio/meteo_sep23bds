f4efab1a0a8ba1061db4746013c6027ab3fce70e
Omar Choa <omar.choa@gmail.com>
2023-12-08 21:34:18 +0100

    Réorganisation en onglets

--- streamlit_app/test_multiple_pages/pages/03_Préparation_des_données.py
+++ streamlit_app/test_multiple_pages/pages/03_Préparation_des_données.py
@@ -2,172 +2,179 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-# = PAGE =
-st.title(":bento: Préparation des données")
-
-st.divider()
-
-
-## == SECTION ==
-st.header(":thinking_face: Quels choix effectuer ?")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Contexte")
-
-st.markdown("Nous devons gérer des **NaN** dans :")
-st.markdown("- 16 variables :violet[numériques]")
-st.markdown("- 7 variables :orange[catégorielles]")
-
-st.markdown("Nous avons identifié **2 options :**")
-st.markdown("- :blue[Remplacement]")
-st.markdown("- :red[Suppression]")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Illustration")
-
-st.markdown(
-    ":blue[Remplacement] des NaN dans les variables de température (:violet[numériques]) à Melbourne :"
+tab_1, tab_2, tab_3 = st.tabs(
+    [
+        ":thinking_face: Quels choix effectuer ?",
+        ":hammer_and_wrench: _Preprocessing_ et _feature engineering_",
+        ":calendar: Phases",
+    ]
 )
-strategie_remplacement_temp = ["Aucune", "`interpolate`", "`mean`"]
-strategie_remplacement_temp_choix = st.radio("Stratégie :", strategie_remplacement_temp)
-st.image(
-    "../../reports/figures/date_vs_temp_Melbourne_{}.png".format(
-        strategie_remplacement_temp_choix
+
+
+with tab_1:
+    # = PAGE =
+    st.title(":bento: Préparation des données")
+
+    ## == SECTION ==
+    st.header(":thinking_face: Quels choix effectuer ?")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Contexte")
+
+    st.markdown("Nous devons gérer des **NaN** dans :")
+    st.markdown("- 16 variables :violet[numériques]")
+    st.markdown("- 7 variables :orange[catégorielles]")
+
+    st.markdown("Nous avons identifié **2 options :**")
+    st.markdown("- :blue[Remplacement]")
+    st.markdown("- :red[Suppression]")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Illustration")
+
+    st.markdown(
+        ":blue[Remplacement] des NaN dans les variables de température (:violet[numériques]) à Melbourne :"
     )
-)
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Illustration")
-
-st.markdown(
-    ":blue[Remplacement] des NaN dans les variables de vent (:violet[numériques] et :orange[catégorielles]) à Sydney :"
-)
-strategie_remplacement_vent = ["`dropna`", "`fillna`"]
-strategie_remplacement_vent_choix = st.radio("Stratégie :", strategie_remplacement_vent)
-st.image(
-    "../../reports/figures/rose_vents_Sydney_{}.png".format(
-        strategie_remplacement_vent_choix
+    strategie_remplacement_temp = ["Aucune", "`interpolate`", "`mean`"]
+    strategie_remplacement_temp_choix = st.radio(
+        "Stratégie :", strategie_remplacement_temp
     )
-)
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Illustration")
-
-st.markdown(
-    ":red[Suppression] des NaN dans les variables pseudo-cible et cible (:orange[catégorielles]) :"
-)
-var_cible = ["`RainToday`", "`RainTomorrow`"]
-var_cible_choix = st.radio("Variable :", var_cible)
-strategie_remplacement_cible = ["Aucune", "`dropna`"]
-strategie_remplacement_cible_choix = st.radio(
-    "Stratégie :", strategie_remplacement_cible
-)
-st.image(
-    "../../reports/figures/distrib_{}_{}.png".format(
-        var_cible_choix, strategie_remplacement_cible_choix
+    st.image(
+        "../../reports/figures/date_vs_temp_Melbourne_{}.png".format(
+            strategie_remplacement_temp_choix
+        )
     )
-)
 
-st.write("\n")
+    st.write("\n")
 
+    ### === SOUS-SECTION ===
+    st.subheader("Illustration")
 
-### === SOUS-SECTION ===
-st.subheader("Résumé")
+    st.markdown(
+        ":blue[Remplacement] des NaN dans les variables de vent (:violet[numériques] et :orange[catégorielles]) à Sydney :"
+    )
+    strategie_remplacement_vent = ["`dropna`", "`fillna`"]
+    strategie_remplacement_vent_choix = st.radio(
+        "Stratégie :", strategie_remplacement_vent
+    )
+    st.image(
+        "../../reports/figures/rose_vents_Sydney_{}.png".format(
+            strategie_remplacement_vent_choix
+        )
+    )
 
-st.markdown("- `Date` : aucune action nécessaire")
-st.markdown("- `Location` : aucune action nécessaire")
-st.markdown(
-    "- Toutes les variables :violet[numériques] sauf `Rainfall` : :blue[remplacement] des NaN par la **moyenne** de chaque station"
-)
-st.markdown(
-    "- `WindGustDir`, `WindDir9am`, `WindDir3pm` : :blue[remplacement] des NaN par la **mode** de chaque station"
-)
-st.markdown("- `RainToday`, `RainTomorrow` : :red[suppression] des NaN")
+    st.write("\n")
 
-st.divider()
+    ### === SOUS-SECTION ===
+    st.subheader("Illustration")
 
+    st.markdown(
+        ":red[Suppression] des NaN dans les variables pseudo-cible et cible (:orange[catégorielles]) :"
+    )
+    var_cible = ["`RainToday`", "`RainTomorrow`"]
+    var_cible_choix = st.radio("Variable :", var_cible)
+    strategie_remplacement_cible = ["Aucune", "`dropna`"]
+    strategie_remplacement_cible_choix = st.radio(
+        "Stratégie :", strategie_remplacement_cible
+    )
+    st.image(
+        "../../reports/figures/distrib_{}_{}.png".format(
+            var_cible_choix, strategie_remplacement_cible_choix
+        )
+    )
 
-## == SECTION ==
-st.header(":hammer_and_wrench: _Preprocessing_ et _feature engineering_")
+    st.write("\n")
 
-st.write("\n")
+    ### === SOUS-SECTION ===
+    st.subheader("Résumé")
 
-
-### === SOUS-SECTION ===
-st.subheader("Variables :violet[numériques]")
-
-st.markdown("- Standardisation")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Variables :orange[catégorielles]")
-
-st.markdown("- `Date` : extraction et enregistrement de l'année, mois et jour")
-st.markdown("- `Location` : encodage des valeurs de `0` à `48`")
-st.markdown(
-    "- `WindGustDir`, `WindDir9am`, `WindDir3pm` : encodage des valeurs en radians"
-)
-st.markdown(
-    "- `RainToday`, `RainTomorrow` : encodage des valeurs par `0` (`No`) ou `1` (`Yes`)"
-)
-
-st.divider()
-
-
-## == SECTION ==
-st.header(":calendar: Phases")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Phase A")
-
-st.markdown("Travail sur **9 sous-tableaux**")
-
-components.html(
-    html="""<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F8aPx8akpCF1HLdDkNdv7Nz%2Fpipeline%3Ftype%3Dwhiteboard%26node-id%3D0%253A1%26t%3DitCNIBp3Eo7fHMvB-1" allowfullscreen></iframe>""",
-    width=800,
-    height=450,
-)
-
-st.write("\n")
+    st.markdown("- `Date` : aucune action nécessaire")
+    st.markdown("- `Location` : aucune action nécessaire")
+    st.markdown(
+        "- Toutes les variables :violet[numériques] sauf `Rainfall` : :blue[remplacement] des NaN par la **moyenne** de chaque station"
+    )
+    st.markdown(
+        "- `WindGustDir`, `WindDir9am`, `WindDir3pm` : :blue[remplacement] des NaN par la **mode** de chaque station"
+    )
+    st.markdown("- `RainToday`, `RainTomorrow` : :red[suppression] des NaN")
 
 
-### === SOUS-SECTION ===
-st.subheader("Phase B")
+with tab_2:
+    # = PAGE =
+    st.title(":bento: Préparation des données")
 
-st.markdown("Retour à **1 tableau unique**")
+    ## == SECTION ==
+    st.header(":hammer_and_wrench: _Preprocessing_ et _feature engineering_")
 
-components.html(
-    html="""<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="640" height="480" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FxnhjGD3NKTM5TSNEoOQmc0%2Fdata_prep_pipeline_1df%3Ftype%3Dwhiteboard%26node-id%3D0%253A1%26t%3DVPZbJJ5vB8r7ow4q-1" allowfullscreen></iframe>""",
-    width=640,
-    height=480,
-)
+    st.write("\n")
 
-st.write("\n")
+    ### === SOUS-SECTION ===
+    st.subheader("Variables :violet[numériques]")
+
+    st.markdown("- Standardisation")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Variables :orange[catégorielles]")
+
+    st.markdown("- `Date` : extraction et enregistrement de l'année, mois et jour")
+    st.markdown("- `Location` : encodage des valeurs de `0` à `48`")
+    st.markdown(
+        "- `WindGustDir`, `WindDir9am`, `WindDir3pm` : encodage des valeurs en radians"
+    )
+    st.markdown(
+        "- `RainToday`, `RainTomorrow` : encodage des valeurs par `0` (`No`) ou `1` (`Yes`)"
+    )
 
 
-### === SOUS-SECTION ===
-st.subheader("Phase C")
+with tab_3:
+    # = PAGE =
+    st.title(":bento: Préparation des données")
 
-st.markdown("Choix de **métrique** et **optimisation**")
-st.markdown("- **Objectif :** 5 prédictions justes sur 6 (83%)")
-st.markdown(
-    "- **Implémentation :** évaluation souple de `accuracy`, `precision` et `recall`"
-)
-st.markdown("- **Optimisation :** stratification, rééchantillonnage, standardisation")
+    ## == SECTION ==
+    st.header(":calendar: Phases")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Phase A")
+
+    st.markdown("Travail sur **9 sous-tableaux**")
+
+    components.html(
+        html="""<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F8aPx8akpCF1HLdDkNdv7Nz%2Fpipeline%3Ftype%3Dwhiteboard%26node-id%3D0%253A1%26t%3DitCNIBp3Eo7fHMvB-1" allowfullscreen></iframe>""",
+        width=800,
+        height=450,
+    )
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Phase B")
+
+    st.markdown("Retour à **1 tableau unique**")
+
+    components.html(
+        html="""<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="640" height="480" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FxnhjGD3NKTM5TSNEoOQmc0%2Fdata_prep_pipeline_1df%3Ftype%3Dwhiteboard%26node-id%3D0%253A1%26t%3DVPZbJJ5vB8r7ow4q-1" allowfullscreen></iframe>""",
+        width=640,
+        height=480,
+    )
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Phase C")
+
+    st.markdown("Choix de **métrique** et **optimisation**")
+    st.markdown("- **Objectif :** 5 prédictions justes sur 6 (83%)")
+    st.markdown(
+        "- **Implémentation :** évaluation souple de `accuracy`, `precision` et `recall`"
+    )
+    st.markdown(
+        "- **Optimisation :** stratification, rééchantillonnage, standardisation"
+    )
--- streamlit_app/test_multiple_pages/pages/05_Séries_temporelles.py
+++ streamlit_app/test_multiple_pages/pages/05_Séries_temporelles.py
@@ -1,175 +1,183 @@
 import streamlit as st
 
 
-# = PAGE =
-st.title(":mantelpiece_clock: Séries temporelles")
-
-st.divider()
-
-
-## == SECTION ==
-st.header(":telescope: Changer d'angle")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Notre jeu de données")
-
-st.markdown("- Observations pour **plusieurs variables** dans le **temps**")
-st.markdown("- Une **série temporelle multivariée**")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Boîte à outils")
-
-st.markdown("- `scikit-learn` : **données tabulaires** (variables i.i.d)")
-st.markdown("- `sktime` : `scikit-learn` pour **séries temporelles**")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Algorithmes de classification retenus")
-
-st.markdown("- Approches **distances :** KNN-DTW")
-st.markdown("- Approches **intervalles :** TSF")
-st.markdown("- Approches **dictionnaires :** ROCKET")
-
-st.divider()
-
-
-## == SECTION ==
-st.header(":spider_web: KNN-DTW")
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Présentation")
-
-st.markdown(
-    "- _K-Nearest Neighbors_ (KNN) avec _Dynamic Time Warping_ (DTW) comme **métrique de distance**"
-)
-st.markdown(
-    "- DTW :  mesure la **similarité** entre séries temporelles avec **vitesses différentes**"
-)
-st.markdown("- 1NN-DTW : _benchmark_ / _gold standard_ ")
-
-st.write("\n")
-
-st.image(
-    "../../reports/figures/series_temp_knn_dtw.png",
-    caption="Source : M. Regan, « K Nearest Neighbors & Dynamic Time Warping ». 5 décembre 2023. Disponible sur : https://github.com/markdregan/K-Nearest-Neighbors-with-Dynamic-Time-Warping",
+tab_1, tab_2, tab_3, tab_4, tab_5 = st.tabs(
+    [
+        ":telescope: Changer d'angle",
+        ":spider_web: KNN-DTW",
+        ":deciduous_tree: TSF",
+        ":rocket: ROCKET",
+        ":gear: _Preprocessing_ +",
+    ]
 )
 
-st.write("\n")
+
+with tab_1:
+    # = PAGE =
+    st.title(":mantelpiece_clock: Séries temporelles")
+
+    ## == SECTION ==
+    st.header(":telescope: Changer d'angle")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Notre jeu de données")
+
+    st.markdown("- Observations pour **plusieurs variables** dans le **temps**")
+    st.markdown("- Une **série temporelle multivariée**")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Boîte à outils")
+
+    st.markdown("- `scikit-learn` : **données tabulaires** (variables i.i.d)")
+    st.markdown("- `sktime` : `scikit-learn` pour **séries temporelles**")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Algorithmes de classification retenus")
+
+    st.markdown("- Approches **distances :** KNN-DTW")
+    st.markdown("- Approches **intervalles :** TSF")
+    st.markdown("- Approches **dictionnaires :** ROCKET")
 
 
-### === SOUS-SECTION ===
-st.subheader("Implémentation")
+with tab_2:
+    # = PAGE =
+    st.title(":mantelpiece_clock: Séries temporelles")
 
-st.code(
-    """from sktime.classification.distance_based import KNeighborsTimeSeriesClassifier
+    ## == SECTION ==
+    st.header(":spider_web: KNN-DTW")
 
-clf = KNeighborsTimeSeriesClassifier(n_neighbors=1, distance="dtw")
+    st.write("\n")
 
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)"""
-)
+    ### === SOUS-SECTION ===
+    st.subheader("Présentation")
 
-st.divider()
+    st.markdown(
+        "- _K-Nearest Neighbors_ (KNN) avec _Dynamic Time Warping_ (DTW) comme **métrique de distance**"
+    )
+    st.markdown(
+        "- DTW :  mesure la **similarité** entre séries temporelles avec **vitesses différentes**"
+    )
+    st.markdown("- 1NN-DTW : _benchmark_ / _gold standard_ ")
 
+    st.write("\n")
 
-## == SECTION ==
-st.header(":deciduous_tree: TSF")
+    st.image(
+        "../../reports/figures/series_temp_knn_dtw.png",
+        caption="Source : M. Regan, « K Nearest Neighbors & Dynamic Time Warping ». 5 décembre 2023. Disponible sur : https://github.com/markdregan/K-Nearest-Neighbors-with-Dynamic-Time-Warping",
+    )
 
-st.write("\n")
+    st.write("\n")
 
+    ### === SOUS-SECTION ===
+    st.subheader("Implémentation")
 
-### === SOUS-SECTION ===
-st.subheader("Présentation")
+    st.code(
+        """from sktime.classification.distance_based import KNeighborsTimeSeriesClassifier
 
-st.markdown("- _Time Series Forest_ (TSF)")
-st.markdown("- Adaptation de _Random Forest_ (RF) aux séries temporelles")
-st.markdown(
-    "- **Entraînement** d'un arbre de décision par **intervalle temporel distinct,** puis **vote d'ensemble**"
-)
+    clf = KNeighborsTimeSeriesClassifier(n_neighbors=1, distance="dtw")
 
-st.write("\n")
-
-st.image(
-    "../../reports/figures/series_temp_tsf.png",
-    caption="Source : H. Deng, G. Runger, E. Tuv, et M. Vladimir, « A time series forest for classification and feature extraction », Information Sciences, vol. 239, p. 142-153, août 2013, doi: 10.1016/j.ins.2013.02.030.",
-    width=480,
-)
-
-st.write("\n")
-
-
-### === SOUS-SECTION ===
-st.subheader("Implémentation")
-
-st.code(
-    """from sktime.classification.interval_based import TimeSeriesForestClassifier
-
-clf = TimeSeriesForestClassifier(n_estimators=200, min_interval=3)
-
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)"""
-)
-
-st.divider()
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)"""
+    )
 
 
-## == SECTION ==
-st.header(":rocket: ROCKET")
+with tab_3:
+    # = PAGE =
+    st.title(":mantelpiece_clock: Séries temporelles")
 
-st.write("\n")
+    ## == SECTION ==
+    st.header(":deciduous_tree: TSF")
 
+    st.write("\n")
 
-### === SOUS-SECTION ===
-st.subheader("Présentation")
+    ### === SOUS-SECTION ===
+    st.subheader("Présentation")
 
-st.markdown("- _RandOm Convolutional KErnel Transform_")
-st.markdown("- S'inspire des **réseaux de neurones convolutifs**")
-st.markdown(
-    "- **Transformation** des séries temporelles à l'aide de noyaux de convolution aléatoires, puis **entraînement** d’un classificateur linéaire"
-)
+    st.markdown("- _Time Series Forest_ (TSF)")
+    st.markdown("- Adaptation de _Random Forest_ (RF) aux séries temporelles")
+    st.markdown(
+        "- **Entraînement** d'un arbre de décision par **intervalle temporel distinct,** puis **vote d'ensemble**"
+    )
 
-st.write("\n")
+    st.write("\n")
 
-st.image(
-    "../../reports/figures/series_temp_rocket.png",
-    caption="Source : « Convolution based time series classification in aeon », aeon. Disponible sur : https://www.aeon-toolkit.org/en/latest/examples/classification/examples/classification/convolution_based.html",
-)
+    st.image(
+        "../../reports/figures/series_temp_tsf.png",
+        caption="Source : H. Deng, G. Runger, E. Tuv, et M. Vladimir, « A time series forest for classification and feature extraction », Information Sciences, vol. 239, p. 142-153, août 2013, doi: 10.1016/j.ins.2013.02.030.",
+        width=480,
+    )
 
-st.write("\n")
+    st.write("\n")
 
+    ### === SOUS-SECTION ===
+    st.subheader("Implémentation")
 
-### === SOUS-SECTION ===
-st.subheader("Implémentation")
+    st.code(
+        """from sktime.classification.interval_based import TimeSeriesForestClassifier
 
-st.code(
-    """from sktime.transformations.panel.rocket import Rocket
-from sklearn.linear_model import SGDClassifier
+    clf = TimeSeriesForestClassifier(n_estimators=200, min_interval=3)
 
-trf = Rocket(num_kernels=10000)
-clf = SGDClassifier(loss="log_loss")
-
-X_train = trf.fit_transform(X_train)
-X_test = trf.transform(X_test)
-
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)"""
-)
-
-st.divider()
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)"""
+    )
 
 
-## == SECTION ==
-st.header(":gear: _Preprocessing_ complémentaire")
+with tab_4:
+    # = PAGE =
+    st.title(":mantelpiece_clock: Séries temporelles")
 
-st.markdown("_Suite_")
+    ## == SECTION ==
+    st.header(":rocket: ROCKET")
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Présentation")
+
+    st.markdown("- _RandOm Convolutional KErnel Transform_")
+    st.markdown("- S'inspire des **réseaux de neurones convolutifs**")
+    st.markdown(
+        "- **Transformation** des séries temporelles à l'aide de noyaux de convolution aléatoires, puis **entraînement** d’un classificateur linéaire"
+    )
+
+    st.write("\n")
+
+    st.image(
+        "../../reports/figures/series_temp_rocket.png",
+        caption="Source : « Convolution based time series classification in aeon », aeon. Disponible sur : https://www.aeon-toolkit.org/en/latest/examples/classification/examples/classification/convolution_based.html",
+    )
+
+    st.write("\n")
+
+    ### === SOUS-SECTION ===
+    st.subheader("Implémentation")
+
+    st.code(
+        """from sktime.transformations.panel.rocket import Rocket
+    from sklearn.linear_model import SGDClassifier
+
+    trf = Rocket(num_kernels=10000)
+    clf = SGDClassifier(loss="log_loss")
+
+    X_train = trf.fit_transform(X_train)
+    X_test = trf.transform(X_test)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)"""
+    )
+
+
+with tab_5:
+    # = PAGE =
+    st.title(":mantelpiece_clock: Séries temporelles")
+
+    ## == SECTION ==
+    st.header(":gear: _Preprocessing_ +")
+
+    st.markdown("_Suite_")
