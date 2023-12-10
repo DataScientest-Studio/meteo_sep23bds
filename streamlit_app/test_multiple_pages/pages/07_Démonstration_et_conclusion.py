a2d80183054c04985b3fcc9416f18ba89678e619
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2023-12-10 20:38:40 +0100

    Update 07_Démonstration_et_conclusion.py

--- streamlit_app/test_multiple_pages/pages/07_Démonstration_et_conclusion.py
+++ streamlit_app/test_multiple_pages/pages/07_Démonstration_et_conclusion.py
@@ -20,19 +20,15 @@ df = pd.read_csv('../../data/processed/model_weatherAUS.csv')
 df.drop(columns = 'Unnamed: 0', inplace = True)
 df['Date'] = pd.to_datetime(df['Date'])
 
-###### Proportions initiales target:
-#RainTomorrow
-#0    0.778382
-#1    0.221618
-
 # Séparation data / target:
 X = df.drop(columns = ['RainTomorrow', 'Date']).copy()
 y = df['RainTomorrow'].copy()
 
 # Séparation du jeu d'entrainement et du jeu de test:
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 123, stratify = y) 
-# stratify = y assure que la proportion initiale de y se retrouve à l'identique dans les deux échantillons de train et test,
-# mais ne permet pas de ré-équilibrer les classes!
+
+# Sauvegarde de test en df:
+Xt, yt = X_test, y_test 
 
 # Scale de X_train, X_test:
 scaler = StandardScaler()
@@ -45,10 +41,10 @@ y_preds = pd.Series(np.where(preds[:, 1] > 0.66, 1, 0))
 
 ################################################################################################
 
-st.title("Projet Météo en Australie")
-st.header("Démonstration et conclusion")
+st.title(":gear: Démonstration et conclusion")
+st.divider()
 
-st.markdown("Voici les résultats pour notre meilleur modèle sur le jeu de test:")
+st.markdown("Voici les résultats pour notre :orange[meilleur modèle] sur le jeu de test:")
 
 
 class_rep = pd.DataFrame.from_dict(classification_report(y_test, y_preds, output_dict=True, digits = 2)).T  # création au format df
@@ -62,8 +58,9 @@ st.markdown(""" Matrice de confusion:
             """)
 st.dataframe(conf_mat)
 
-clic = st.button("Faire une prédicition!")
-if clic == True :
+########################################### Bouton Prédiction n°1 #############################################
+clic1 = st.button("Faire une prédicition!")
+if clic1 == True :
     n = np.random.randint(X_test.shape[0])
     pred = rdf.predict_proba((X_test[n].reshape(1, -1))) # il faut reshape le vecteur à tester. X_test[n].shape = (28, ) et rdf.predict attend un array de dimension 2.
     y_pred = pd.Series(np.where(pred[:, 1] > 0.66, 1, 0))
@@ -78,6 +75,31 @@ if clic == True :
     else:
         st.write("Cette prédiction est incorrecte... :x:")
 
+############################################ Boutton prédictions n°2 ###########################################
+coefficients = rdf.feature_importances_
+feats_i = pd.DataFrame({'Feature' : X.columns, 'Importance' : np.abs(coefficients)})
+feats_i = feats_i.sort_values('Importance', ascending = False)
+new_col = list(feats_i['Feature'])
 
+clic2 = st.button("Essayons de prédire nous-même!")
+if clic2 == True :
+    n = np.random.choice(Xt.index) # choix d'une ligne par index dans Xt
 
-                
+    ech = Xt.loc[[n]] # double crochets pour avoir un df
+    test = scaler.transform(Xt.loc[[n]])
+    pred = rdf.predict_proba(test)
+    y_pred = pd.Series(np.where(pred[:, 1] > 0.66, 1, 0))
+
+    ech = ech.reindex(columns=new_col)
+    st.dataframe(ech)
+
+    if y_pred.array[0] == 1:
+        st.write("On prévoit de la pluie :rain_cloud: demain :umbrella_with_rain_drops:.")
+    else:
+        st.write("On prévoit du beau temps :sun_with_face: demain :sunglasses:  .")
+
+    if y_pred.array[0] == yt[n]:  # il faut convertir y_test en array pour pouvoir appeler le nombre correspondant à la ligne n.
+        st.write("Cette prédiction est exacte! :heavy_check_mark:")
+    else:
+        st.write("Cette prédiction est incorrecte... :x:")
+                    
