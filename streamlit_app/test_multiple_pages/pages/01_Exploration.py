27bdc8b609303f0353562b5ced628ca84312638f
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2023-12-10 20:36:34 +0100

    Update 01_Exploration.py

--- streamlit_app/test_multiple_pages/pages/01_Exploration.py
+++ streamlit_app/test_multiple_pages/pages/01_Exploration.py
@@ -10,9 +10,8 @@ var_num = list(df.select_dtypes(include = float).columns)
 # Liste des stations:
 liste_stations = list(df['Location'].unique())
 
-st.title("Projet Météo en Australie")
-
-st.write("### Introduction")
+st.title(":mag: Exploration")
+st.divider()
 
 # Tentative d'affichage de la page html "stations.html": ne marche pas.
 # station_map = open("../../data/external/stations.html", "r")
@@ -22,29 +21,29 @@ st.write("### Introduction")
 # source_code = station_map.close()
 
 # Affichage de la carte d'Australie avec les stations:
-st.markdown("""Notre tableau contient des données provenant de 49 stations différentes:""")
+st.markdown("""Notre tableau contient des données provenant de :orange[49 stations différentes]:""")
 image = "../../data/external/carte_stations.jpg"
 st.image(image, caption = "Carte des stations")
    
 
 # Présentation du tableau:
-st.markdown("""Le tableau est constitué de relevés quotidiens de grandeurs physiques 
-                mesurées par chaque station, sur une durée d'environ 10 ans, entre 2007 et 2017.""")
+st.markdown("""Le tableau est constitué de :orange[relevés quotidiens] de grandeurs physiques 
+                mesurées :violet[par chaque station], sur une durée :blue[d'environ 10 ans, entre 2007 et 2017].""")
 st.dataframe(df.head(10))
 
-st.markdown("""Il contient environ 150 000 lignes, et 23 colonnes. Le fichier fait 25.5 MB.""")
+st.markdown("""Il contient environ :orange[150 000 lignes], et :violet[23 colonnes]. Le fichier fait 25.5 MB.""")
 st.write(df.shape)
 
-st.markdown("""Variable cible: `RainTomorrow`. Il s'agit de prévoir s'il pleuvra le lendemain en se basant sur les mesures de la journée.  
-                Cette variable est déséquilibrée: il fait beau 78 \% du temps, et il pleut les 22 \% restants.""")
+st.markdown("""Variable cible: `RainTomorrow`. Il s'agit de prévoir s'il pleuvra le lendemain en se basant sur les mesures de la journée.
+                Cette variable est :orange[déséquilibrée]: il fait beau 78 \% du temps, et il pleut les 22 \% restants.""")
 
 if st.checkbox("Afficher les infos"):
-    st.markdown("""Il y a 16 variables quantitatives et 7 variables qualitatives:""")
+    st.markdown("""Il y a :orange[16 variables quantitatives] et :violet[7 variables qualitatives]:""")
     buffer = io.StringIO()
     df.info(buf=buffer)
     s = buffer.getvalue()
     st.text(s)
 
 if st.checkbox("Afficher le pourcentage de  NaN"):
-    st.markdown("""Certaines grandeurs sont massivement absentes, comme `Evaporation` ou `Sunshine`.""")
+    st.markdown("""Certaines grandeurs sont :red[massivement absentes], comme `Evaporation` ou `Sunshine`.""")
     st.dataframe(df.isna().sum()/df.shape[0]*100)
