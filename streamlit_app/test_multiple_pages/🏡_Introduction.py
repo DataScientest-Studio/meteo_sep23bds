65d7de60015c1d7300d6f1b14ffa3dc57f366f58
Omar Choa <omar.choa@gmail.com>
2023-12-11 13:29:19 +0100

    Finalisation de la page d'accueil

--- streamlit_app/test_multiple_pages/🏡_Introduction.py
+++ streamlit_app/test_multiple_pages/🏡_Introduction.py
@@ -6,6 +6,14 @@ st.set_page_config(
 )
 
 
+# = SIDEBAR =
+with st.sidebar:
+    st.header(":partly_sunny: Prévision météo en Australie")
+    st.markdown(
+        "Un projet de _data science_ réalisé par [Alexandre Winger](https://github.com/alexandrewinger) et [Omar Choa](https://www.linkedin.com/in/omarchoa/)."
+    )
+
+
 # = PAGE =
 st.title(":partly_sunny: Prévision météo en Australie")
 
