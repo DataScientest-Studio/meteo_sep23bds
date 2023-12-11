f23d940a5ef4e6ccd9369a436731fabdc3f5060a
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2023-12-11 14:02:35 +0100

    Update 07_⚙️_Démonstration_et_conclusion.py

--- streamlit_app/test_multiple_pages/pages/07_⚙️_Démonstration_et_conclusion.py
+++ streamlit_app/test_multiple_pages/pages/07_⚙️_Démonstration_et_conclusion.py
@@ -62,25 +62,17 @@ st.divider()
 ## == SECTION ==
 st.header("Résultats")
 
-st.markdown(
-    "Voici les résultats pour notre :orange[meilleur modèle] sur le jeu de test:"
-)
+st.markdown("Voici les résultats pour notre :orange[meilleur modèle] sur le jeu de test:")
 
 class_rep = pd.DataFrame.from_dict(
     classification_report(y_test, y_preds, output_dict=True, digits=2)
 ).T  # création au format df
 conf_mat = confusion_matrix(y_test, y_preds)  # Création au format df
 
-st.markdown(
-    """ Rapport de classification:
-            """
-)
+st.markdown("""Rapport de classification:""")
 st.dataframe(class_rep)
 
-st.markdown(
-    """ Matrice de confusion:
-            """
-)
+st.markdown(""" Matrice de confusion:""")
 st.dataframe(conf_mat)
 
 ############################################ Boutton prédictions n°2 ###########################################
@@ -93,29 +85,48 @@ feats_i = pd.DataFrame({"Feature": X.columns, "Importance": np.abs(coefficients)
 feats_i = feats_i.sort_values("Importance", ascending=False)
 new_col = list(feats_i["Feature"])
 
-n = np.random.choice(Xt.index)  # choix d'une ligne par index dans Xt
+clic = st.button("Essayons de prédire nous-même!")
+if clic == True :
+    n = np.random.choice(Xt.index)  # choix d'une ligne par index dans Xt
 
-ech = Xt.loc[[n]]  # double crochets pour avoir un df
-test = scaler.transform(Xt.loc[[n]])
-pred = rdf.predict_proba(test)
-y_pred = pd.Series(np.where(pred[:, 1] > 0.66, 1, 0))
+    ech = Xt.loc[[n]]  # double crochets pour avoir un df
+    test = scaler.transform(Xt.loc[[n]])
+    pred = rdf.predict_proba(test)
+    y_pred = pd.Series(np.where(pred[:, 1] > 0.66, 1, 0))
 
-ech = ech.reindex(columns=new_col)
+    ech = ech.reindex(columns=new_col)
 
-st.markdown(
-    """ Voici un échantillon tiré aléatoirement parmi l'ensemble de test.
-            Les colonnes sont triées par ordre d'importances estimées par le modèle.
-            """
-)
-st.dataframe(ech)
+    st.markdown("""
+                Voici un échantillon tiré aléatoirement parmi l'ensemble de test.
+                Les colonnes sont triées par ordre d'importances estimées par le modèle.
+                """)
 
-if st.checkbox("Afficher le résultat"):
+    st.dataframe(ech)
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+    st.write("\n")
+
     if y_pred.array[0] == 1:
-        st.write(
-            "On prévoit de la pluie :rain_cloud: demain :umbrella_with_rain_drops:."
-        )
+        st.write("On prévoit de la pluie :rain_cloud: demain :umbrella_with_rain_drops:.")
     else:
-        st.write("On prévoit du beau temps :sun_with_face: demain :sunglasses:  .")
+        st.write("On prévoit du beau temps :sun_with_face: demain :sunglasses: .")
 
     if (
         y_pred.array[0] == yt[n]
